d2f44ec536f04257cd1ecaf6f065fdddcea64647
Checkmate50 <dgeisler50@gmail.com>
2020-02-23 18:11:02 -0500

    Fixed looping issue

--- pathfinding/pathFinding.py
+++ pathfinding/pathFinding.py
@@ -5,12 +5,12 @@ from itertools import combinations
 
 _GRAPH = \
 [[0, 1, 0],
- [1, 0, 0],
- [0, 0, 0]]
+ [1, 0, 1],
+ [0, 1, 0]]
 
 # Find and return all paths from s to t in the given graph
-def findPaths(s, t, graph = _GRAPH):
-    return getList(s, t, graph, set())
+def findPathsReference(s, t, graph = _GRAPH):
+    return getList(s, t, graph)
 
 # A dict of all paths in the graph
 def findAllPaths(graph = _GRAPH):
@@ -19,13 +19,13 @@ def findAllPaths(graph = _GRAPH):
         if len(graph[i]) != len(graph):
             raise "Graph must be a square matrix"
         for j in range(len(graph[i])):
-            p = findPaths(i, j, graph)
+            p = findPathsReference(i, j, graph)
             if len(p) > 0:
                 toReturn["(" + str(i) + ", " + str(j) + ")"] = p
     return toReturn
 
 # A list of _all_ pairs of paths that share a source and sink
-def findConflictingPaths(graph = _GRAPH):
+def findConflictingPathsReference(graph = _GRAPH):
     result = []
     allPaths = findAllPaths(graph)
     for sources in allPaths:
@@ -34,10 +34,10 @@ def findConflictingPaths(graph = _GRAPH):
 
 # A list of all pairs of paths that both share a source and sink
 #  and include 'edge' in the path
-def findNewConflicts(edge, graph = _GRAPH):
+def findNewConflictsReference(edge, graph = _GRAPH):
     result = []
     graph[edge[0]][edge[1]] = 1
-    for path in findConflictingPaths(graph):
+    for path in findConflictingPathsReference(graph):
         for i in range(2):
             for j in range(len(path[i]) - 1):
                 if path[i][j] == edge[0] and path[i][j + 1] == edge[1]:
@@ -46,17 +46,22 @@ def findNewConflicts(edge, graph = _GRAPH):
 
 # ---------- Helper functions ----------
 
-def getList(s, t, graph, visited, first_node=True):
+def getList(s, t, graph):
+    return getListRec(s, t, graph, set(), -1)
+
+def getListRec(s, t, graph, visited, first_node):
     """Return a list of paths from s to t
        Note that first_node allows us to get loops"""
-    if s==t and not first_node:
+    if s==t and not first_node == -1:
         return [[t]]
+    if first_node == -1:
+        first_node = s
     neighbours = getNeighbours(s, graph, visited)
     ans = []
     for neighbour in neighbours:
-        if not first_node:
+        if not s == t:
             visited.add(s)
-        ans += [[s]+lst for lst in getList(neighbour, t, graph, visited, False)]
+        ans += [[s]+lst for lst in getListRec(neighbour, t, graph, visited, first_node)]
         visited.discard(neighbour)
     return ans + ([[t]] if s==t else [])
 
@@ -74,9 +79,9 @@ def printDict(d):
 def main():
     printDict(findAllPaths())
     print("-------------------")
-    print(findConflictingPaths())
+    print(findConflictingPathsReference())
     print("-------------------")
-    print(findNewConflicts((2, 1)))
+    print(findNewConflictsReference((0, 2)))
 
 if __name__=="__main__":
     main()
