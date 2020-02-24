6767b9053a37b4a7c3bb1f044224945b2e21d9d8
Checkmate50 <dgeisler50@gmail.com>
2020-02-24 01:21:32 -0500

    Baseline pathfinding seems to work

--- pathfinding/pathFinding.py
+++ pathfinding/pathFinding.py
@@ -4,13 +4,35 @@
 from itertools import combinations
 
 _GRAPH = \
-[[0, 1, 0],
- [1, 0, 1],
- [0, 1, 0]]
+[[0, 1],
+ [0, 0]]
+
+# _GRAPH = \
+# [[0, 1, 1, 1, 0, 1],
+#  [0, 0, 1, 1, 1, 1],
+#  [0, 1, 0, 0, 0, 0],
+#  [1, 1, 0, 0, 1, 0],
+#  [1, 0, 0, 1, 0, 1],
+#  [0, 1, 1, 1, 1, 0],]
+
+# ----------- Fast functions -----------
+
+def findPaths(s, t, graph = _GRAPH):
+    return [[0]]
+
+def findAllConflictingPaths(graph = _GRAPH):
+    return [[(0, 0)]]
+
+def findNewConflicts(edge, graph = _GRAPH):
+    result = [(0, 0)]
+    graph[edge[0]][edge[1]] = 1
+    return result
+
+# -------- Reference functions ---------
 
 # Find and return all paths from s to t in the given graph
 def findPathsReference(s, t, graph = _GRAPH):
-    return getList(s, t, graph)
+    return getDirectedPaths(s, t, graph)
 
 # A dict of all paths in the graph
 def findAllPaths(graph = _GRAPH):
@@ -25,7 +47,7 @@ def findAllPaths(graph = _GRAPH):
     return toReturn
 
 # A list of _all_ pairs of paths that share a source and sink
-def findConflictingPathsReference(graph = _GRAPH):
+def findAllConflictingPathsReference(graph = _GRAPH):
     result = []
     allPaths = findAllPaths(graph)
     for sources in allPaths:
@@ -37,7 +59,7 @@ def findConflictingPathsReference(graph = _GRAPH):
 def findNewConflictsReference(edge, graph = _GRAPH):
     result = []
     graph[edge[0]][edge[1]] = 1
-    for path in findConflictingPathsReference(graph):
+    for path in findAllConflictingPathsReference(graph):
         for i in range(2):
             for j in range(len(path[i]) - 1):
                 if path[i][j] == edge[0] and path[i][j + 1] == edge[1]:
@@ -46,28 +68,40 @@ def findNewConflictsReference(edge, graph = _GRAPH):
 
 # ---------- Helper functions ----------
 
-def getList(s, t, graph):
-    return getListRec(s, t, graph, set(), -1)
+# Returns a list of all directed paths from s to t
+# Directed paths may include up to 'flips_allowed' direction flips
+def getDirectedPaths(s, t, graph, flips_allowed = 0):
+    return getDirectedPathsRec(s, t, graph, set(), -1, True, flips_allowed)
 
-def getListRec(s, t, graph, visited, first_node):
-    """Return a list of paths from s to t
-       Note that first_node allows us to get loops"""
+def getDirectedPathsRec(s, t, graph, visited, first_node, forward, flips_allowed):
     if s==t and not first_node == -1:
         return [[t]]
     if first_node == -1:
         first_node = s
-    neighbours = getNeighbours(s, graph, visited)
+    elif s == first_node:
+        return []
+    else:
+        visited.add(s)
+    neighbours = getNeighbours(s, graph, visited, forward)
     ans = []
+    forwardVisited = visited.copy()
     for neighbour in neighbours:
-        if not s == t:
-            visited.add(s)
-        ans += [[s]+lst for lst in getListRec(neighbour, t, graph, visited, first_node)]
-        visited.discard(neighbour)
+        ans += [[s]+lst for lst in \
+            getDirectedPathsRec(neighbour, t, graph, visited.copy(), first_node, forward, flips_allowed)]
+        forwardVisited.add(neighbour)
+    if flips_allowed > 0:
+        flips_allowed -= 1
+        forward = not forward
+        flippedNeighbors = getNeighbours(s, graph, forwardVisited, forward)
+        for neighbour in flippedNeighbors:
+            ans += [[s]+lst for lst in \
+                getDirectedPathsRec(neighbour, t, graph, visited.copy(), first_node, forward, flips_allowed)]
     return ans + ([[t]] if s==t else [])
 
 # Return a list of the neighbours of s that haven't been visited
-def getNeighbours(s, graph, visited):
-    lst = filter(lambda x : x not in visited and graph[s][x]==1, range(len(graph[s])))
+def getNeighbours(s, graph, visited, forward):
+    graph_check = (lambda x : graph[s][x]==1) if forward else (lambda x : graph[x][s]==1)
+    lst = filter(lambda x : x not in visited and graph_check(x), range(len(graph[s])))
     return lst
 
 # Print a dictionary line-by-line (not very pythonic, but whatever)
@@ -79,9 +113,9 @@ def printDict(d):
 def main():
     printDict(findAllPaths())
     print("-------------------")
-    print(findConflictingPathsReference())
+    print(findAllConflictingPathsReference())
     print("-------------------")
-    print(findNewConflictsReference((0, 2)))
+    print(findNewConflictsReference((1, 0)))
 
 if __name__=="__main__":
     main()
